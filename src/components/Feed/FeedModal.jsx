0ce70a293e6a351c1bb93d2f60058010808b6f5f
vmcustodio <van.custodio.vc@gmail.com>
2023-12-05 23:20:22 -0300

    feature: add modal

--- src/components/Feed/FeedModal.jsx
+++ src/components/Feed/FeedModal.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import styles from "./FeedModal.module.css";
+import useFetch from "../../Hooks/useFetch";
+import { PHOTO_GET } from "../../api";
+import Error from "../Helpers/Error";
+import Loading from "../Helpers/Loading";
+import PhotoContent from "../Photo/PhotoContent";
 
-const FeedModal = () => {
-  return <div>aa</div>;
+const FeedModal = ({ photo }) => {
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
+  return (
+    <div className={styles.modal}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
 };
 
 export default FeedModal;
