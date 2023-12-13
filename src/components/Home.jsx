452a687271f5056ee56aa7e2dea3311be06eccc9
vmcustodio <van.custodio.vc@gmail.com>
2023-12-12 23:33:08 -0300

    feat: add head

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Feed from "./Feed/Feed";
 
 import styles from "./Footer.module.css";
+import Head from "./Helpers/Head";
 
 const Home = () => {
   return (
     <section className="container mainContainer">
+      <Head title="Fotos" />
       <Feed></Feed>
     </section>
   );
